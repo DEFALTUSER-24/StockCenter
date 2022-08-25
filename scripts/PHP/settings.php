14848f0885f70c33677bb70fcee5fb1e6cec69b8
DEFALTUSER-24 <ezequiel.castro38@hotmail.com>
2022-08-24 21:08:26 -0300

    Settings to default.

--- scripts/PHP/settings.php
+++ scripts/PHP/settings.php
@@ -1,10 +1,10 @@
 <?php
-    const G_RECAPTCHA_PRIVATE = "6Lc5EaYhAAAAAPXspYRlgJaAkPLYuifeWPvqn-PG";
-    const G_RECAPTCHA_PUBLIC = "6Lc5EaYhAAAAAK0LwxQJ_i7lWsiW_Kv1ew4LLl1Q";
+    const G_RECAPTCHA_PRIVATE = "";
+    const G_RECAPTCHA_PUBLIC = "";
 
     const ENCRYPT_KEY = "";
 
-    const DB_HOST = "localhost";
-    const DB_USERNAME = "root";
+    const DB_HOST = "";
+    const DB_USERNAME = "";
     const DB_PASSWORD = "";
-    const DB_NAME = "stock_center";
+    const DB_NAME = "";
